df1d6f8d3fef263bc5e60a792c391e5ef5d24376
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2026-07-23 22:56:19 +0100

    Add Google Tag Manager snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SocialShareFab } from "@/components/organisms/SocialShareFab";
 import "./globals.css";
 
@@ -20,6 +21,15 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MZKR9HNB');
+          `}
+        </Script>
         <meta name="google-site-verification" content="iK6qKM3LeQO-QEeOrEiZfRjxMjKk6KQnNlt1pL5r4wQ" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -29,6 +39,14 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZKR9HNB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <SocialShareFab />
       </body>
